18d65a758fa33dfc30d26da75dd605eae82b13ee
Justine Bungabong <justinebungabon@gmail.com>
2026-09-22 20:28:10 +0800

    1.3: course information step 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,13 @@ const Content = (props) => {
   return (
     <div>
       <p>
-        {props.part1}
+        {props.part1.name} {props.part1.units}
       </p>
       <p>
-        Units: {props.units1}
+        {props.part2.name} {props.part2.units}
       </p>
       <p>
-        {props.part2}
-      </p>
-      <p>
-        Units: {props.units2}
-      </p>
-      <p>
-        {props.part3}
-      </p>
-      <p>
-        Units: {props.units3}
+        {props.part3.name} {props.part3.units}
       </p>
     </div>
   )
@@ -30,7 +21,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <p>
-      Total number of units: {props.units1 + props.units2 + props.units3}
+      Total number of units: {props.part1.units + props.part2.units + props.part3.units}
     </p>
   )
 }
@@ -47,37 +38,30 @@ const Footer = (props) => {
 
 const App = () => {
   const course = 'Bachelor of Science in Information Technology'
-  const part1 = 'CSIT340 - Industry Elective 1'
-  const units1 = 3
-  const part2 = 'IT365 - Data Analytics 1'
-  const units2 = 3
-  const part3 = 'CSIT327 - Information Management 2'
-  const units3 = 3 
+
+  const part1 = {
+    name: 'CSIT340 - Industry Elective 1',
+    units: 3
+  }
+  const part2 = {
+    name: 'IT365 - Data Analytics 1',
+    units: 3
+  }
+  const part3 = {
+    name: 'CSIT327 - Information Management 2',
+    units: 3
+  }
+
   const studentName = 'Justine T. Bungabong'
   const courseCode = 'CSIT340'
   const section = 'G8'
+
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        units1={units1}
-        part2={part2}
-        units2={units2}
-        part3={part3}
-        units3={units3}
-      />
-      <Total
-        units1={units1}
-        units2={units2}
-        units3={units3}
-      />
-
-      <Footer
-        studentName={studentName}
-        courseCode={courseCode}
-        section={section}
-      />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
+      <Footer studentName={studentName} courseCode={courseCode} section={section} />
     </div>
   )
 }
